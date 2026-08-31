85d3e53f08f25186a56f0cbefe468ca930e3fdd3
Artyom Yenin <artyomenin05@yandex.ru>
2026-08-31 16:02:55 +0300

    The ConnectionManager architecture has been changed due to incorrect operation in the case of multiple running sessions for a single user. The work with Redis has been optimized.

--- redis_manager/redis.py
+++ redis_manager/redis.py
@@ -1,5 +1,5 @@
 import redis.asyncio as redis
-
+from typing import List
 
 class RedisManager:
     def __init__(self):
@@ -29,15 +29,42 @@ class RedisManager:
             raise RuntimeError("Redis is not connected. Call connect() first.")
         return self.redis
 
+    async def subscribe_users_to_chat(self, user_ids: List[int], chat_id: int):
+        client = self.get_client()
+        async with client.pipeline(transaction=False) as pipe:
+            for user_id in user_ids:
+                pipe.sadd(f"chat:{chat_id}:subscribers", str(user_id))
+                pipe.sadd(f"user:{user_id}:chats", f"chat:{chat_id}")
+            await pipe.execute()
+
+    async def subscribe_user_to_chats(self, user_id: int, chat_ids: List[int]):
+        client = self.get_client()
+        async with client.pipeline(transaction=False) as pipe:
+            for chat_id in chat_ids:
+                pipe.sadd(f"chat:{chat_id}:subscribers", str(user_id))
+                pipe.sadd(f"user:{user_id}:chats", f"chat:{chat_id}")
+            await pipe.execute()
+
     async def subscribe_user_to_chat(self, user_id: int, chat_id: int):
         client = self.get_client()
         await client.sadd(f"chat:{chat_id}:subscribers", str(user_id))
         await client.sadd(f"user:{user_id}:chats", f"chat:{chat_id}")
 
-    async def unsubscribe_user_from_chat(self, user_id: int, chat_id: int):
+    async def unsubscribe_user_from_chats(self, user_id: int, chat_ids: List[int]):
         client = self.get_client()
-        await client.srem(f"chat:{chat_id}:subscribers", str(user_id))
-        await client.srem(f"user:{user_id}:chats", f"chat:{chat_id}")
+        async with client.pipeline(transaction=False) as pipe:
+            for chat_id in chat_ids:
+                pipe.srem(f"chat:{chat_id}:subscribers", str(user_id))
+                pipe.srem(f"user:{user_id}:chats", f"chat:{chat_id}")
+            await pipe.execute()
+
+    async def unsubscribe_users_from_chat(self, users_id: List[int], chat_id: int):
+        client = self.get_client()
+        async with client.pipeline(transaction=False) as pipe:
+            for user_id in users_id:
+                pipe.srem(f"chat:{chat_id}:subscribers", str(user_id))
+                pipe.srem(f"user:{user_id}:chats", f"chat:{chat_id}")
+            await pipe.execute()
 
     async def get_chat_subscribers(self, chat_id: int) -> set[str]:
         client = self.get_client()
--- websocket/ws.py
+++ websocket/ws.py
@@ -1,5 +1,6 @@
 import http
-
+import uuid
+from typing import List, Set
 from fastapi import WebSocket, Depends, APIRouter, status, WebSocketDisconnect
 from fastapi.exceptions import HTTPException
 from redis_manager.redis import redis_manager
@@ -12,33 +13,47 @@ import json
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: dict[int, WebSocket] = {}
+        self.active_connections: dict[str, WebSocket] = {} #словарь {uuid : websocket}
+        self.active_sessions: dict[int, Set[str]] = {} #cловарь {user_id: Set[uuid]}
+
+    async def add_new_chat(self, user_ids: List[int], chat_id: int):
+        online_users = [user_id for user_id in user_ids if (user_id in self.active_sessions and self.active_sessions[user_id])]
+        await redis_manager.subscribe_users_to_chat(online_users, chat_id)
 
     async def connect(self, websocket: WebSocket, db: AsyncSession, user_id: int):
-        self.active_connections[user_id] = websocket
-        chat_ids = await self.get_user_chats_from_db(user_id, db)
-        for chat_id in chat_ids:
-            await redis_manager.subscribe_user_to_chat(user_id, chat_id)
+        session = uuid.uuid4().hex
+        self.active_connections[session] = websocket
+        if user_id in self.active_sessions:
+            self.active_sessions[user_id].add(session)
+        else:
+            self.active_sessions[user_id] = {session}
+            chat_ids = await self.get_user_chats_from_db(user_id, db)
+            await redis_manager.subscribe_user_to_chats(user_id, chat_ids)
+        return session
 
-    async def disconnect(self, user_id: int):
-        if user_id in self.active_connections:
-            del self.active_connections[user_id]
-        user_chats_key = f"user:{user_id}:chats"
-        chat_keys = await redis_manager.get_client().smembers(user_chats_key)
-        for chat_key in chat_keys:
-            chat_id = chat_key.split(":")[1]
-            await redis_manager.unsubscribe_user_from_chat(user_id, int(chat_id))
+    async def disconnect(self, user_id: int, session: str):
+        if session in self.active_connections:
+            del self.active_connections[session]
+        if user_id in self.active_sessions and session in self.active_sessions[user_id]:
+            self.active_sessions[user_id].remove(session)
+            if not self.active_sessions[user_id]:
+                user_chats_key = f"user:{user_id}:chats"
+                chat_keys = await redis_manager.get_client().smembers(user_chats_key)
+                chat_ids = [int(chat_key.split(":")[1]) for chat_key in chat_keys]
+                if chat_ids:
+                    await redis_manager.unsubscribe_user_from_chats(user_id, chat_ids)
 
     async def send_notify(self, chat_id: int, message_data: dict):
         subscribers = await redis_manager.get_chat_subscribers(chat_id)
         for subscriber_id_str in subscribers:
             user_id = int(subscriber_id_str)
-            if user_id in self.active_connections:
-                try:
-                    data = json.dumps(message_data)
-                    await self.active_connections[user_id].send_text(data)
-                except Exception:
-                    await self.disconnect(user_id)
+            if user_id in self.active_sessions:
+                for session in self.active_sessions[user_id]:
+                    try:
+                        data = json.dumps(message_data)
+                        await self.active_connections[session].send_text(data)
+                    except Exception:
+                        await self.disconnect(user_id, session)
 
     async def get_user_chats_from_db(self, user_id: int, db: AsyncSession) -> list[int]:
         request = select(ChatMember.chat_id).where(ChatMember.user_id == user_id)
@@ -58,8 +73,7 @@ async def websocket_endpoint(
         db: AsyncSession = Depends(get_db)
 ):
     user_id = None
-    is_connected = False
-
+    session = None
     try:
         await websocket.accept()
         user_id = await get_current_user_ws(websocket)
@@ -67,9 +81,8 @@ async def websocket_endpoint(
         if not user_id:
             await websocket.close(code=4001, reason="Unauthorized")
             return
-        await manager.connect(websocket, db, user_id)
-        is_connected = True
-
+        print("Starting Connection")
+        session = await manager.connect(websocket, db, user_id)
         print(f"User {user_id} connected successfully via Cookies")
         while True:
             data = await websocket.receive_text()
@@ -85,6 +98,6 @@ async def websocket_endpoint(
         print(f"Something went wrong with user {user_id}: {e}")
 
     finally:
-        if is_connected and user_id:
-            await manager.disconnect(user_id)
+        if session and user_id:
+            await manager.disconnect(user_id, session)
 
